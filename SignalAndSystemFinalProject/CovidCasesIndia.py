c5378467b2f89c2deb769480c2190f602ddca6a4
Rushil Borad <rushilborad@gmail.com>
2023-02-21 11:52:44 +0530

    Delete SignalAndSystemFinalProject directory

--- SignalAndSystemFinalProject/CovidCasesIndia.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-url_conf = 'F:/Drive D Lenovo_S540/ECE210 Signal and Sustem/Project/COVID_19_Cases_Conf.csv'
-data_conf = pd.read_csv(url_conf)
-
-plt.figure(figsize=(20,5))
-plt.plot(data_conf['Month'],data_conf['Covid Confirmed Cases in India'])
-print(data_conf)
-plt.show()
--- SignalAndSystemFinalProject/CovidRecovIndia.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-url_recov = 'F:/Drive D Lenovo_S540/ECE210 Signal and Sustem/Project/COVID_19_Cases_revor.csv'
-data_recov = pd.read_csv(url_recov)
-plt.figure(figsize=(20,5))
-plt.plot(data_recov['Month'],data_recov['Covid Cured in India'])
-print(data_recov)
-plt.show()
